4cf2f4a20d7655ddf30a898d3edb46537f6d2f2d
theArnoll <s10054@ms2.kjes.kh.edu.tw>
2026-09-13 13:25:46 +0800

    Add Linux build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Build Linux / Unix (requires C++23)"
+echo -------------------------------------
+cd "/home/blackbox/remoteDevelopment/C++/TCtypingTest/" && g++ -std=c++23 main.cpp -o main.out -lstdc++exp && "/home/blackbox/remoteDevelopment/C++/TCtypingTest/"main.out
